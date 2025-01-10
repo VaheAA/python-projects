8ded1e8958edb4cb85d27570cb278145295d7e0d
VaheAA <vahe.abov@gmail.com>
2025-01-10 12:12:20 +0400

    finish guess game

--- number_gessing.py
+++ number_gessing.py
@@ -14,12 +14,22 @@ random_number = random.randrange(top_of_range)
 attempts = 0
 
 while True:
-    answer = int(input("Name your number: "))
+    attempts += 1
+    user_guess = input("Name your number: ")
 
-    if random_number == answer:
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Type a number larger than 0 next time.")
+        continue
+
+    if random_number == user_guess:
         print(f"Correct! You guessed it in {attempts} attempts.")
         break
     else:
-        print("Incorrect, try again")
-    attempts += 1
-    continue    
+        if user_guess > random_number:
+            print("You were above the number!")
+        else:
+            print("You were below the number")
+
+    continue
